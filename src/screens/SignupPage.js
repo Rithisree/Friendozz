7f714a4a2fc15755910d712e63ffae5c37de19ef
Badri238 <badri82301@gmail.com>
2022-09-25 18:34:44 +0530

    signup

--- src/screens/SignupPage.js
+++ src/screens/SignupPage.js
@@ -5,7 +5,7 @@ import { Box, Button, Flex, Stack } from '@react-native-material/core'
 const SignupPage = ({ navigation }) => {
     return (
         <Box w={"100%"} h={"100%"} style={styles.view1} >
-            <Box h={"70%"} w={"100%"} style={styles.view3}>
+            <Box h={"60%"} w={"100%"} style={styles.view3}>
 
                 <Text style={{ marginLeft: 20, marginBottom: -50 }}>FriendOzz.com</Text>
                 <Image
@@ -14,7 +14,62 @@ const SignupPage = ({ navigation }) => {
                 />
 
             </Box>
-        </Box>
+
+            <Box style={{
+                backgroundColor: "white", width: "79%", marginLeft: 10,
+                marginTop: 5,
+                borderRadius: 20,
+                borderWidth: 1,
+                padding: 5
+            }} >
+                <Box style={{ padding: 5, display: "flex", flexDirection: "row", width: "100%", alignItems: "center" }}>
+                    <Image
+
+                        source={require("../assets/goog.png")}
+                    />
+                    <Text style={{ color: "black", fontWeight: "bold", marginLeft: 30, fontSize: 18 }}>Enter With Google</Text>
+                </Box>
+            </Box>
+
+            <Box style={{
+                backgroundColor: "#009DF5", marginRight: 40,
+                marginLeft: 40,
+                borderRadius: 20,
+                borderWidth: 1, width: "80%", marginTop: 20
+            }} >
+                <Box style={{ padding: 5, display: "flex", flexDirection: "row", width: "100%", alignItems: "center" }}>
+                    <Image
+                        style={{
+                            width: 40,
+                            height: 40,
+                            resizeMode: 'contain',
+
+                        }}
+                        source={require("../assets/facebook.png")}
+                    />
+                    <Text style={{ color: "black", marginLeft: 30, fontSize: 18, fontWeight: "bold" }}>Enter With Facebook</Text>
+                </Box>
+            </Box>
+            <Box style={{
+                backgroundColor: "#009DF5", marginRight: 40,
+                marginLeft: 40,
+                borderRadius: 20,
+                borderWidth: 1, width: "80%", marginTop: 20
+            }} >
+                <Box style={{ padding: 5, display: "flex", flexDirection: "row", width: "100%", alignItems: "center" }}>
+
+                    <Text style={{ color: "black", marginLeft: 20, fontSize: 18, padding: 5, fontWeight: "bold", }}>Log in with Phone number/Email</Text>
+                </Box>
+            </Box>
+
+            <Box style={{ width: "70%", alignItems: "center" }}>
+                <Text style={{ color: "white", marginTop: 10, fontFamily: "verdana", fontSize: 18, fontWeight: "bold", }}>Don’t have an account? <Text style={{ color: "#31FB5D" }}>Log In</Text></Text>
+                <Text style={{ color: "white", marginTop: 10, fontFamily: "verdana", fontSize: 14 }}>
+                    By Signing up, you agree to our terms &
+                </Text>
+                <Text style={{ color: "white", fontFamily: "verdana", fontSize: 14, }}>Conditions, privacy.</Text>
+            </Box>
+        </Box >
     )
 }
 
@@ -25,7 +80,7 @@ const styles = StyleSheet.create({
         display: 'flex',
         alignItems: "center",
         backgroundColor: "black",
-        justifyContent: "center"
+        padding: 5
 
     },
     view2: {
@@ -33,7 +88,8 @@ const styles = StyleSheet.create({
         marginBottom: 20
     },
     view3: {
-        justifyContent: "center"
+        justifyContent: "center",
+        marginTop: 5
     },
     icon1: {
         marginLeft: 10,
