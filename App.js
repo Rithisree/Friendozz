d91407f399246122fd47ac82b83f213fd8c0456c
rithisreesathaiyan <rithisreesathaiyan@gmail.com>
2022-10-12 10:25:32 +0530

    firebase

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import RootNavigator from './src/navigations/RootNavigator'
+import TestFirebase from './src/screens/TestFirebase'
+
 import "react-native-gesture-handler"
 const App = () => {
   return (
     <View style={styles.container}>
-      <RootNavigator />
+      <TestFirebase/>
     </View>
 
   )
